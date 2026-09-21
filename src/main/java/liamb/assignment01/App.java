f6357a5c93479ad14327e4daafc8db70f1fff467
liamb2535004 <lbohdjal@gmail.com>
2026-09-20 20:15:22 -0400

    (keycode, button) map and .setOnKeyPressed and.setOnKeyReleased event handlers

--- src/main/java/liamb/assignment01/App.java
+++ src/main/java/liamb/assignment01/App.java
@@ -1,11 +1,13 @@
 package liamb.assignment01;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -104,6 +106,65 @@ public class App extends Application {
                 
         Button spacebarBtn = new Button("space");
         
+        HashMap<KeyCode, Button> keyMap = new HashMap<>();
+        keyMap.put(KeyCode.BACK_QUOTE, backtickBtn);
+        keyMap.put(KeyCode.DIGIT1, btn1);
+        keyMap.put(KeyCode.DIGIT2, btn2);
+        keyMap.put(KeyCode.DIGIT3, btn3);
+        keyMap.put(KeyCode.DIGIT4, btn4);
+        keyMap.put(KeyCode.DIGIT5, btn5);
+        keyMap.put(KeyCode.DIGIT6, btn6);
+        keyMap.put(KeyCode.DIGIT7, btn7);
+        keyMap.put(KeyCode.DIGIT8, btn8);
+        keyMap.put(KeyCode.DIGIT9, btn9);
+        keyMap.put(KeyCode.DIGIT0, btn0);
+        keyMap.put(KeyCode.MINUS, minusBtn);
+        keyMap.put(KeyCode.EQUALS, equalsBtn);
+        keyMap.put(KeyCode.BACK_SPACE, deleteBtn);
+        
+        keyMap.put(KeyCode.TAB, tabBtn);
+        keyMap.put(KeyCode.Q, qBtn);
+        keyMap.put(KeyCode.W, wBtn);
+        keyMap.put(KeyCode.E, eBtn);
+        keyMap.put(KeyCode.R, rBtn);
+        keyMap.put(KeyCode.T, tBtn);
+        keyMap.put(KeyCode.Y, yBtn);
+        keyMap.put(KeyCode.U, uBtn);
+        keyMap.put(KeyCode.I, iBtn);
+        keyMap.put(KeyCode.O, oBtn);
+        keyMap.put(KeyCode.P, pBtn);
+        keyMap.put(KeyCode.OPEN_BRACKET, leftBracketBtn);
+        keyMap.put(KeyCode.CLOSE_BRACKET, rightBracketBtn);
+        keyMap.put(KeyCode.BACK_SLASH, backslashBtn);
+        
+        keyMap.put(KeyCode.CAPS, capsLockBtn);
+        keyMap.put(KeyCode.A, aBtn);
+        keyMap.put(KeyCode.S, sBtn);
+        keyMap.put(KeyCode.D, dBtn);
+        keyMap.put(KeyCode.F, fBtn);
+        keyMap.put(KeyCode.G, gBtn);
+        keyMap.put(KeyCode.H, hBtn);
+        keyMap.put(KeyCode.J, jBtn);
+        keyMap.put(KeyCode.K, kBtn);
+        keyMap.put(KeyCode.L, lBtn);
+        keyMap.put(KeyCode.SEMICOLON, semiColonBtn);
+        keyMap.put(KeyCode.QUOTE, apostropheBtn);
+        keyMap.put(KeyCode.ENTER, enterBtn);
+
+        keyMap.put(KeyCode.SHIFT, leftShiftBtn);
+        keyMap.put(KeyCode.Z, zBtn);
+        keyMap.put(KeyCode.X, xBtn);
+        keyMap.put(KeyCode.C, cBtn);
+        keyMap.put(KeyCode.V, vBtn);
+        keyMap.put(KeyCode.B, bBtn);
+        keyMap.put(KeyCode.N, nBtn);
+        keyMap.put(KeyCode.M, mBtn);
+        keyMap.put(KeyCode.COMMA, commaBtn);
+        keyMap.put(KeyCode.PERIOD, periodBtn);
+        keyMap.put(KeyCode.SLASH, forwardSlashBtn);
+        
+        keyMap.put(KeyCode.SPACE, spacebarBtn);
+        
         if (typeGuide.getText() == textField.getText()) {
             nextBtn.setDisable(false);
         }
@@ -150,7 +211,35 @@ public class App extends Application {
          
         VBox root = new VBox(5);
         root.getChildren().addAll(topRow, textField, row1, row2, row3, row4, row5, statusLabel);
+        
         var scene = new Scene(root, 900, 400);
+        
+        textField.setOnKeyPressed(event -> {
+            KeyCode code = event.getCode();
+            
+            if (keyMap.containsKey(code)) {
+                Button btn = keyMap.get(code);
+                
+                if (!btn.getStyleClass().contains("pressed")) {
+                    btn.getStyleClass().add("pressed");
+                }
+                statusLabel.setText("Pressed: " + code.getName());
+                statusLabel.setStyle("-fx-text-fill: green;");
+            } else {
+                statusLabel.setText("Not handled");
+                statusLabel.setStyle("-fx-text-fill: red;");
+            }
+        });
+
+        textField.setOnKeyReleased(event -> {
+            KeyCode code = event.getCode();
+            if (keyMap.containsKey(code)) {
+                Button btn = keyMap.get(code);
+                btn.getStyleClass().remove("pressed");
+            }
+        });
+        
+        textField.requestFocus();
         scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
